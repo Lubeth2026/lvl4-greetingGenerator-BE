a5bbbaacb7c2c7066e6bfd71a45d15cf5fc2ce1d
Lubeth <lubethjimenez@yahoo.com>
2026-06-28 15:39:53 -0400

    Add a greetings route in app.py to retrieve the name saved, read it, add it to a greeting, and display it

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+import random
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
     return "Hello from the Greeting Generator backend!"
 
+@app.route("/greetings", methods=["POST"])
+def greeting():
+    data = request.json
+    name = data["name"]
+
+    with open("greeting.json") as file:
+        greetings_data = json.load(file)
+
+    greeting_template = random.choice(greetings_data['greetings'])
+    message = greeting_template.replace( "{name}", name )
+
+    return jsonify({"message": message})
